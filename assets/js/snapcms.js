2ef8004ba5de9959decd8fc1eee873ad3a31b918
Jackson Tong <jackson@snapfrozen.com.au>
2015-12-04 08:58:38 +0700

    fixed ajax lightbox in pjax grid table

--- assets/js/snapcms.js
+++ assets/js/snapcms.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     // to load ajax lightbox
-    $('a.get-ajax').click(function(e) {
+    $(document).on('click', 'a.get-ajax', function(e) {
         e.preventDefault();
         $.getJSON($(this).attr('href'), function (data) {
             $('div#ajax-area').html(data);
